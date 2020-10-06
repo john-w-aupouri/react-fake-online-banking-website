43f4ed6fb5c9e77368bb6f0cede1c79763983fa4
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-10-07 00:39:40 +1300

    landing page markup done

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin, FaYoutube } from 'react-icons/fa'
 import {
   FooterContainer,
   FooterWrap,
@@ -6,7 +7,13 @@ import {
   FooterLinksWrapper,
   FooterLinkItems,
   FooterLinkTitle,
-  FooterLink
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink,
+  WebsiteRights
 } from './FooterElements'
 
 const Footer = () => {
@@ -25,35 +32,57 @@ const Footer = () => {
               <FooterLink to="/login">Terms of service</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/login">How it works</FooterLink>
-              <FooterLink to="/login">Testimonials</FooterLink>
-              <FooterLink to="/login">Careers</FooterLink>
-              <FooterLink to="/login">Investors</FooterLink>
-              <FooterLink to="/login">Terms of service</FooterLink>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/login">Contact</FooterLink>
+              <FooterLink to="/login">Support</FooterLink>
+              <FooterLink to="/login">Destinations</FooterLink>
+              <FooterLink to="/login">Sponsorship</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
 
           <FooterLinksWrapper>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/login">How it works</FooterLink>
-              <FooterLink to="/login">Testimonials</FooterLink>
-              <FooterLink to="/login">Careers</FooterLink>
-              <FooterLink to="/login">Investors</FooterLink>
-              <FooterLink to="/login">Terms of service</FooterLink>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/login">Submit Videos</FooterLink>
+              <FooterLink to="/login">Ambassadors</FooterLink>
+              <FooterLink to="/login">Agency</FooterLink>
+              <FooterLink to="/login">Influencer</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/login">How it works</FooterLink>
-              <FooterLink to="/login">Testimonials</FooterLink>
-              <FooterLink to="/login">Careers</FooterLink>
-              <FooterLink to="/login">Investors</FooterLink>
-              <FooterLink to="/login">Terms of service</FooterLink>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/login">Instagram</FooterLink>
+              <FooterLink to="/login">Facebook</FooterLink>
+              <FooterLink to="/login">Youtube</FooterLink>
+              <FooterLink to="/login">Twitter</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
-          
+
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>
+
+            </SocialLogo>
+            <WebsiteRights> ©{new Date().getFullYear()} All rights reserved.</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   )
